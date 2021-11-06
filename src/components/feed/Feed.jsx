85aaeb6c091bbbf293cc5d78dac1a1b985742ae8
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-06 17:59:37 -0400

    create share component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,4 +1,13 @@
+import Share from "./../share/Share";
+import Post from "./../post/Post";
+
 import "./feed.css";
+
 export default function Feed() {
-  return <div className="feed">FEED</div>;
+  return (
+    <div className="feed">
+      <Share />
+      <Post />
+    </div>
+  );
 }
