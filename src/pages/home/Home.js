a7bcea247005c641db442834365e4d08b8fa3b1a
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-06 17:46:26 -0400

    add sidebar

--- src/pages/home/Home.js
+++
@@ -1,4 +0,0 @@
-import Topbar from "../../components/topbar/Topbar.jsx";
-export default function Home() {
-  return <Topbar />;
-}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import Topbar from "../../components/topbar/Topbar.jsx";
+import Feed from "../../components/feed/Feed.jsx";
+import Sidebar from "../../components/sidebar/Sidebar.jsx";
+import Rightbar from "../../components/rightbar/Rightbar.jsx";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
+}
